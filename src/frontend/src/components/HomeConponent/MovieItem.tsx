e6810c4e8dbac92828ed78dee8b65842507f25b7
mjj <mackenziejhou@gmail.com>
2025-04-17 14:50:57 -0700

    Remove Unused icons

--- src/frontend/src/components/HomeConponent/MovieItem.tsx
+++ src/frontend/src/components/HomeConponent/MovieItem.tsx
@@ -8,15 +8,9 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Rating from '@mui/material/Rating';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 interface Props {
   movie: Movie;
@@ -66,11 +60,6 @@ const MovieItem: React.FC<Props> = ({ movie, addComment }) => {
               fontWeight: 'bold'
             }
           }}
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
           title={movie.title}
           subheader={
             <div>
@@ -95,9 +84,6 @@ const MovieItem: React.FC<Props> = ({ movie, addComment }) => {
         <CardActions disableSpacing>
           <IconButton aria-label="add to favorites">
             <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="share">
-            <ShareIcon />
           </IconButton>
         </CardActions>
       </Card>
